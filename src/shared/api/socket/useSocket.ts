1a145483766310ca9821b12368f32515160dbc85
Choi Chan Hwan <africakokiri@gmail.com>
2026-02-08 06:54:54 +0900

    fix: useSocket hook 성능 개선 및 주석 추가
    - HANDLERS 객체를 useMemo로 wrapping했습니다.
    - useEffect hook의 cleanup 단계에서 socket.disconnect()를 호출하고, socket ref를 null로 변경하도록 수정했습니다.

--- src/shared/api/socket/useSocket.ts
+++ src/shared/api/socket/useSocket.ts
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import { Socket, io } from "socket.io-client";
 
 import { SOCKET_EVENTS } from "@/shared/constants/socketEvents";
@@ -15,12 +15,18 @@ export const useSocket = (accessToken: string) => {
   const [isConnected, setIsConnected] = useState(false);
   const socketRef = useRef<Socket | null>(null);
 
-  const HANDLERS = {
-    [SOCKET_EVENTS.CONNECT]: () => setIsConnected(true),
-    [SOCKET_EVENTS.DISCONNECT]: () => setIsConnected(false),
-    [SOCKET_EVENTS.ERROR]: (error: Err) => console.error(error)
-  };
+  const HANDLERS = useMemo(
+    () => ({
+      [SOCKET_EVENTS.CONNECT]: () => setIsConnected(true),
+      [SOCKET_EVENTS.DISCONNECT]: () => setIsConnected(false),
+      [SOCKET_EVENTS.ERROR]: (error: Err) => console.error(error)
+    }),
+    []
+  );
 
+  // accessToken이 변경되면 내부 코드 재실행
+  // cleanup 단계에서 socket events를 비활성화하고, socket instance를 종료하며, socket ref를 null로 변경함
+  // 따라서 accessToken이 변경되었을 때 새로운 socket instance를 생성할 수 있음
   useEffect(() => {
     if (!accessToken) return;
 
@@ -38,6 +44,9 @@ export const useSocket = (accessToken: string) => {
       Object.entries(HANDLERS).forEach(([event, handler]) => {
         socket.off(event, handler);
       });
+
+      socket.disconnect();
+      socketRef.current = null;
     };
   }, [accessToken]);
 

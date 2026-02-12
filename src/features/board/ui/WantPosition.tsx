87d873533886473c35e4a728ab1965f6d279570d
Choi Chan Hwan <africakokiri@gmail.com>
2026-02-12 18:40:38 +0900

    fix: 시맨틱 태그 적용

--- src/features/board/ui/WantPosition.tsx
+++ src/features/board/ui/WantPosition.tsx
@@ -8,7 +8,7 @@ export function WantPosition() {
       className="flex flex-col items-center justify-center gap-2 rounded-xl border border-gray-300
 bg-white py-4"
     >
-      <p className="bold-12">내가 찾는 포지션</p>
+      <h4 className="bold-12">내가 찾는 포지션</h4>
 
       <div className="flex gap-8">
         <SelectPosition
